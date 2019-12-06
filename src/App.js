7d42fca0ff45c4b72949518e6d0dcd43ab36e86e
publizm <publizm@gmail.com>
2019-12-06 17:42:30 +0900

    docs: youtube api connecting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
+  async getYoutubeData(query = '여행') {
+    const data = await Axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCT5YNj0WpEUrt_4K8b3GZ6NoBZTOImXMA&q=${query}&part=snippet`); // 실제 데이터
+    this.setState({ data });
+    console.log(data);
+  }
+
+  componentDidMount() {
+    this.getYoutubeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
-
-export default App;
+  export default App;
