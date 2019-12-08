371a43c9ab22e2709716dd137a1892d836cb288f
publizm <publizm@gmail.com>
2019-12-08 17:51:26 +0900

    feat: add to search input function

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ import Axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      data: null,
+    };
+    this.searchVideo = this.searchVideo.bind(this);
   }
 
   async getYoutubeData(query = '여행') {
-    const data = await Axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCT5YNj0WpEUrt_4K8b3GZ6NoBZTOImXMA&q=${query}&part=snippet`); // 실제 데이터
-    this.setState({ data });
-    console.log(data);
+    try {
+      const data = await Axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCT5YNj0WpEUrt_4K8b3GZ6NoBZTOImXMA&q=${query}&part=snippet`); // 실제 데이터
+      this.setState({data: data.data});
+    } catch(e) {
+      console.error(e);
+    }
   }
 
   componentDidMount() {
     this.getYoutubeData();
   }
 
+  searchVideo({ target, keyCode }) {
+    const value = target.value.trim();
+
+    if (value === '' || keyCode !== 13) return;
+
+    this.getYoutubeData(value);
+  }
+
   render() {
+    console.log('render', this.state.data);
     return (
-      <div className="App">
-      </div>
+      <>
+        <div className="App">
+          <input type="text" onKeyUp={this.searchVideo} />
+        </div>
+      </>
     );
   }
 }
