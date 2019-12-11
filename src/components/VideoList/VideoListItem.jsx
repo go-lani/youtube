37023f7303ee6622eddf6cec8554c60a27dc32e3
publizm <publizm@gmail.com>
2019-12-11 20:58:36 +0900

    docs: modify VideoListItem logic

--- src/components/VideoList/VideoListItem.jsx
+++ src/components/VideoList/VideoListItem.jsx
@@ -10,23 +10,28 @@ const VideoListItem = props => {
   //   channel: props.videoInfo.snippet.channelTitle,
   //   id: props.videoInfo.id.videoId,
   // }
+
+  const videoItems = props.videoInfo.map(video =>
+    <li className="video-items" key={uuid.v4()} onClick={(e) => props.onVideoSelect(video)}>
+      <a href="#self">
+        <figure className="items-inner">
+          <div className="thumbs">
+            <img src={video.snippet.thumbnails.high.url} alt={video.snippet.title} />
+          </div>
+          <figcaption className="caption">
+            <div className="title">{video.snippet.title}</div>
+            <div className="description">{video.snippet.description}</div>
+          </figcaption>
+        </figure>
+      </a>
+    </li>
+  );
+
   return (
-    props.videoInfo.map(video =>
-      <li className="video-items" key={uuid.v4()} onClick={(e) => props.onVideoSelect(video)}>
-        <a href="#self">
-          <figure className="items-inner">
-            <div className="thumbs">
-              <img src={video.snippet.thumbnails.high.url} alt={video.snippet.title} />
-            </div>
-            <figcaption className="caption">
-              <div className="title">{video.snippet.title}</div>
-              <div className="description">{video.snippet.description}</div>
-            </figcaption>
-          </figure>
-        </a>
-      </li>
-    )
-  )
+    <>
+      {videoItems}
+    </>
+  );
 }
 
 export default VideoListItem;
