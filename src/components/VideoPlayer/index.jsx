f8d832278c33726e1bffcfeeb06e80f953ddec31
publizm <publizm@gmail.com>
2019-12-15 13:59:04 +0900

    feat: separate Main.js and App.js

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -3,28 +3,31 @@ import './VideoPlayer.css';
 
 const VideoPlayer = props => {
   console.log(props);
-  const videoData = {
-    id: props.selectedVideo.id.videoId,
-    title: props.selectedVideo.snippet.title,
-    description: props.selectedVideo.snippet.description,
-    img: props.selectedVideo.snippet.thumbnails.high.url,
-    channel: props.selectedVideo.snippet.channelTitle,
-  }
-  if (!videoData.id) return null;
-  const url = `https://www.youtube.com/embed/${videoData.id}`
+  // const videoData = {
+  //   id: props.selectedVideo.id.videoId,
+  //   title: props.selectedVideo.snippet.title,
+  //   description: props.selectedVideo.snippet.description,
+  //   img: props.selectedVideo.snippet.thumbnails.high.url,
+  //   channel: props.selectedVideo.snippet.channelTitle,
+  // }
+
+  // if (!videoData.id) return null;
+
+  // const url = `https://www.youtube.com/embed/${videoData.id}`
   return (
-    <section className="video-section">
-      <figure className="video-area">
-        <div className="video-box">
-          <iframe src={url} title={videoData.title}></iframe>
-        </div>
-        <figcaption className="video-info">
-          <div className="channel-title">{videoData.channel}</div>
-          <div className="title">{videoData.title}</div>
-          <div className="description">{videoData.description}</div>
-        </figcaption>
-      </figure>
-    </section>
+    // <section className="video-section">
+    //   <figure className="video-area">
+    //     <div className="video-box">
+    //       <iframe src={url} title={videoData.title}></iframe>
+    //     </div>
+    //     <figcaption className="video-info">
+    //       <div className="channel-title">{videoData.channel}</div>
+    //       <div className="title">{videoData.title}</div>
+    //       <div className="description">{videoData.description}</div>
+    //     </figcaption>
+    //   </figure>
+    // </section>
+    <div></div>
   );
 };
 
