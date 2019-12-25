90f360ff216ab144a8d798c23c7f8949ff82a7c3
publizm <publizm@gmail.com>
2019-12-25 15:46:28 +0900

    docs: first build

---
+++ build/precache-manifest.f3890beb73ac252bb75741f71bd17914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4b82d41495e5f98891a2680fc3ae83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd2d4d9f0c48e4ee925",
+    "url": "/static/css/main.7131273e.chunk.css"
+  },
+  {
+    "revision": "78393607bf26b1a533a4",
+    "url": "/static/js/2.f016b9a8.chunk.js"
+  },
+  {
+    "revision": "cfd2d4d9f0c48e4ee925",
+    "url": "/static/js/main.5accfe9a.chunk.js"
+  },
+  {
+    "revision": "722067bf925fd38ce6eb",
+    "url": "/static/js/runtime-main.10cc9ac0.js"
+  },
+  {
+    "revision": "0848e58a05c16e902ed5682509c76711",
+    "url": "/static/media/youtube-logo.0848e58a.png"
+  }
+]);
